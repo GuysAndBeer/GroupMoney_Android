e1aadbde9cb54ceebd6fbcd4d4dc4baacec9ec3b
saba <saba@MacBook-Air.local>
2021-11-13 21:43:06 +0300

    add authentication

---
+++ SplitwiseClone/app/src/main/java/com/example/splitwiseclone/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.splitwiseclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn = findViewById(R.id.button6);
+
+
+        Button logout = findViewById(R.id.logoutbtn);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
+            }
+        });
+
+        Button profile = findViewById(R.id.profile);
+
+    }
+}
